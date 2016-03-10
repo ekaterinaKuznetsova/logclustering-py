665289e49c753b0288c207adfe9e5b0f9591aa16
cliu <fluency.03@gmail.com>
2016-03-10 21:53:58 +0100

    backup code

---
+++ backup.py
@@ -0,0 +1,66 @@
+
+#13
+def levenshtein(s1, s2):
+    '''
+    Dynamic Programming algorithm, with the added optimization that 
+    Only the last two rows of the dynamic programming matrix are needed for the computation
+    '''
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return (float(previous_row[-1]) / float(max(len(s1), len(s2))))
+
+#10
+def minimumEditDistance(s1,s2):
+    if len(s1) > len(s2):
+        s1,s2 = s2,s1
+    distances = range(len(s1) + 1)
+    for index2,char2 in enumerate(s2):
+        newDistances = [index2+1]
+        for index1,char1 in enumerate(s1):
+            if char1 == char2:
+                newDistances.append(distances[index1])
+            else:
+                newDistances.append(1 + min((distances[index1],
+                                             distances[index1+1],
+                                             newDistances[-1])))
+        distances = newDistances
+    #return distances[-1]
+    return (float(distances[-1]) / float(max(len(s1), len(s2))))
+
+#16
+def levenshteinDistance(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    lensum = float(m + n)
+    d = []           
+    for i in range(m+1):
+        d.append([i])        
+    del d[0][0]    
+    for j in range(n+1):
+        d[0].append(j)       
+    for j in range(1,n+1):
+        for i in range(1,m+1):
+            if str1[i-1] == str2[j-1]:
+                d[i].insert(j,d[i-1][j-1])           
+            else:
+                minimum = min(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1]+2)         
+                d[i].insert(j, minimum)
+    ldist = d[-1][-1]
+    ratio = (lensum - ldist)/lensum
+    #return {'distance':ldist, 'ratio':ratio}
+    return (float(ldist) / float(max(len(str1), len(str1))))
