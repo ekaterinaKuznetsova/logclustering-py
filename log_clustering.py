378d40cd422fe3de82b55a4c74c3a88e67c3078d
cliu <fluency.03@gmail.com>
2016-03-12 20:02:25 +0100

    add class LogTemplateExtractor

--- log_clustering.py
+++ log_clustering.py
@@ -1,79 +1,82 @@
-'''
+"""
 This program is used to cluster logs based on their similarity.
 The similarity between two logs is measured based the editing distance between.
 The basic unit of a log is not a character but a token.
 
-[1] Wikibooks: Algorithm Implementation/Strings/Levenshtein distance - Python Code:
+[1] Wikibooks: Algorithm Implementation/Strings/Levenshtein distance - Python:
 https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
 
 [2] Fast implementation of the edit distance(Levenshtein distance)
 https://github.com/aflc/editdistance
 
-The method 'levenshteinVec(source, target)' implemented here is very slower than the package 'editdistance',
-Since the source code of 'editdistance' is written in C++
-However, if we would like to modify the levenshtein algorithm by adding weights to different token classes
-'levenshteinVec(source, target)' is easier to change the code
+The method 'levenshteinVec(source, target)' implemented here is very slower than
+the package 'editdistance', since the source code of 'editdistance' is written
+in C++. However, if we would like to modify the levenshtein algorithm by adding
+weights to different token classes 'levenshteinVec(source, target)' is easier to
+change the code
+"""
 
-TODO: output sequence.
-'''
 
-import sys
+import editdistance
 import numpy as np
 import re
+import sys
 import time
-import editdistance
 
 
-
-class LogTemplateExtractor:
-    '''
+class LogTemplateExtractor(object):
+    """
     A log template extractor
-    '''
+    """
     def __init__(self, logfile):
         self.logfile = logfile
         self.outfile = "/home/cliu/Documents/SC-1/output"
         self.console = "/home/cliu/Documents/SC-1/console"
 
         # delimiters for dividing a log into tokens
-        self.delimiter = r'[ ,:()\[\]=|/\\{}\'\"]+' # ,:()[]=|\/{}'"          no <>
+        self.delimiter = r'[ ,:()\[\]=|/\\{}\'\"]+'  # ,:()[]=|\/{}'"    no <>
 
-        # two logs with editing distance less than distance_threshold are considered to be similar
+        # two logs with editing distance less than distance_threshold
+        # are considered to be similar
         self.distance_threshold = 0.3
 
-        # how many chars are ignored from the beginning of a log (because of the time-stamp, server-name, etc.)
+        # how many chars are ignored from the beginning of a log
+        # (because of the time-stamp, server-name, etc.)
         self.ignored_chars = 21
 
     def setLogFile(self, logfile):
-        '''
+        """
         Set the source log file (name/path) which is going to be analyzed.
-        '''
+        """
         self.logfile = logfile
 
     def setDelimiter(self, delimiter):
-        '''
-        Set the delimiters (in regular expression) for dividing one log into tokens.
-        '''
+        """
+        Set the delimiters (in regular expression)
+        for dividing one log into tokens.
+        """
         self.delimiter = delimiter
 
     def setDistanceThreshold(self, distance_threshold):
-        '''
-        Set the distance threshold used for creating new cluster. The value range is 0 ~ 1.
-        The less the threshold is, the more similar two logs have to be if they want to be clustered together.
-        '''
+        """
+        Set the distance threshold 0 ~ 1 used for creating new cluster.
+        The less the threshold is, the more similar two logs have to be
+        if they want to be clustered together.
+        """
         self.logfile = distance_threshold
 
     def setIgnoredChars(self, ignored_chars):
-        '''
+        """
         Set the ignored chars at the beginning of each log.
-        '''
+        """
         self.ignored_chars = ignored_chars
 
     def levenshteinVec(self, source, target):
-        '''
+        """
         Dynamic Programming algorithm, with the added optimization that only the
-        last two rows of the dynamic programming matrix are needed for the computation.
-        Vectorized version using NumPy.
-        '''
+        last two rows of the dynamic programming matrix are needed for the
+        computation. Vectorized version using NumPy.
+        """
         if len(source) < len(target):
             return self.levenshteinVec(target, source)
 
@@ -108,12 +111,12 @@ class LogTemplateExtractor:
 
             previous_row = current_row
 
-        return (float(previous_row[-1]) / float(max(len(source), len(target))))
+        return float(previous_row[-1]) / float(max(len(source), len(target)))
 
     def isTimeStamp(self, input_str):
-        '''
+        """
         Check whether input str is with time format like: Feb 11 05:22:51 .
-        '''
+        """
         try:
             time.strptime(input_str, '%b %d %H:%M:%S ')
             return True
@@ -121,16 +124,21 @@ class LogTemplateExtractor:
             return False
 
     def minDistance(self, added_line, cluster_dict):
-        '''
+        """
         Calculate the minimal distance between the log and all the clusters.
         Return the minimal distance and its index.
-        '''
+        """
         distance = []
-        added_line_tokens = self.replaceNumByWildcard(re.split(self.delimiter, added_line[self.ignored_chars:]))
+        added_line_tokens = self.replaceNumByWildcard(
+            re.split(self.delimiter, added_line[self.ignored_chars:]))
         for cluster_num in cluster_dict:
             cluster = cluster_dict[cluster_num]
-            cluster_line_tokens = self.replaceNumByWildcard(re.split(self.delimiter, cluster[0][self.ignored_chars:]))
-            dis_ratio = (float(editdistance.eval(cluster_line_tokens, added_line_tokens)) / float(max(len(added_line_tokens), len(cluster_line_tokens))))
+            cluster_line_tokens = self.replaceNumByWildcard(
+                re.split(self.delimiter, cluster[0][self.ignored_chars:]))
+            dis_ratio = (float(editdistance.eval(cluster_line_tokens,
+                                                 added_line_tokens)) /
+                         float(max(len(added_line_tokens),
+                                   len(cluster_line_tokens))))
             distance.append(dis_ratio)
         # print distance
         min_index = np.argmin(distance)
@@ -139,24 +147,28 @@ class LogTemplateExtractor:
         return min_dis, min_index
 
     def replaceNumByWildcard(self, tokens):
-        '''
-        Replace number tokens and hex (starts with 0x...) tokens by wildcard symbol * .
-        '''
+        """
+        Replace number tokens and hex (0x...) tokens by wildcard symbol * .
+        """
         for i in range(0, len(tokens)):
-            if tokens[i].isdigit() or re.search(r'0x[\da-fA-F]', tokens[i]) is not None:
+            if (tokens[i].isdigit() or
+                re.search(r'0x[\da-fA-F]', tokens[i]) is not None):
                 tokens[i] = '*'
         return tokens
 
     def partitionByCommand(self):
-        '''
-        First partition the original logs based on their command type, for two reasons:
-        1. Dramatically reduce the computational time, especially plenty of time spent on levenshtein distance.
-        2. Naturally, we should cluster logs starting with different command names into different clusters.
-        '''
+        """
+        First partition the original logs based on their command type because:
+        1. Dramatically reduce the computational time, especially plenty of time
+         spent on levenshtein distance.
+        2. Naturally, we should cluster logs starting with different command
+         names into different clusters.
+        """
         command_cluster = {}
         # pattern for extracting the command.
         # the command token could contain English letters, '-', '_' and '.'
-        # example: rsyslogd, CMW, ERIC-RDA-Merged-Campaign, mmas_syslog_control_setup.sh, etc.
+        # example: rsyslogd, CMW, ERIC-RDA-Merged-Campaign,
+        # mmas_syslog_control_setup.sh, etc.
         pattern = re.compile(r'([\w\-\_\.]+)([\[:])(.*)')
 
         with open(self.logfile) as f:
@@ -203,9 +215,9 @@ class LogTemplateExtractor:
         return command_cluster
 
     def logClusteringWithPrePartition(self):
-        '''
-        Similarity checks and further clustering after partitioning based on command.
-        '''
+        """
+        Similarity checks and clustering after partitioning based on command.
+        """
         command_cluster = self.partitionByCommand()
         cluster_dict = {}
 
@@ -215,7 +227,8 @@ class LogTemplateExtractor:
                     if not cluster_dict:
                         cluster_dict[0] = [line]
                     else:
-                        min_dis, min_index = self.minDistance(line, cluster_dict)
+                        min_dis, min_index = self.minDistance(line,
+                                                              cluster_dict)
                         if min_dis < self.distance_threshold:
                             cluster_dict[min_index].append(line)
                         else:
@@ -230,10 +243,10 @@ class LogTemplateExtractor:
         return cluster_dict
 
     def logClustering(self):
-        '''
+        """
         Log clustering without pre-partitioning based on command.
         This is much slower than logClusteringWithPrePartition().
-        '''
+        """
 
         cluster_dict = {}
 
@@ -253,7 +266,8 @@ class LogTemplateExtractor:
                             cluster_dict[0] = [added_line]
                         else:
                             #cluster_dict[len(cluster_dict)] = [added_line]
-                            min_dis, min_index = self.minDistance(added_line, cluster_dict)
+                            min_dis, min_index = self.minDistance(added_line,
+                                                                  cluster_dict)
                             if min_dis < self.distance_threshold:
                                 cluster_dict[min_index].append(added_line)
                             else:
@@ -281,9 +295,9 @@ class LogTemplateExtractor:
         return cluster_dict
 
     def discoverTemplate(self):
-        '''
+        """
         Abstract the template representation from each of the clusters.
-        '''
+        """
         cluster_dict = self.logClusteringWithPrePartition()
 
         with open(self.console, 'w') as c:
@@ -291,12 +305,10 @@ class LogTemplateExtractor:
                 c.write(str(i) + '\n')
                 for item in cluster_dict[i]:
                     c.write(item)
-        # TODO
+        # TODO(fluency03): to be finished
 
 
 
-
-# main function
 def main():
 
     logfile = "/home/cliu/Documents/SC-1/messages"
@@ -311,11 +323,12 @@ def main():
     print "--- %s seconds ---" % (stop_time - start_time)
 
 
-    # ------------------------------ For debugging ------------------------------ #
+    # ---------------------------- For debugging ---------------------------- #
     # list1 = ['a', 'b', 'c', 'd']
     # list2 = ['a', 'b', 'd', 'c', 'e']
     # print levenshteinVec(list1, list2)
-    # str1 = "SC-1 ecimswm: ActivateUpgradePackage::doAct`~ion: com<>pleted 2 of 2 procedures (100 percent)"
+    # str1 = ("SC-1 ecimswm: ActivateUpgradePackage::doAct`~ion: "
+    #         "com<>pleted 2 of 2 procedures (100 percent)")
     # print replaceNumByWildcard(re.split(delimiter, str1))
     # string1 = 'adqe!f-'
     # string2 = 'adqef-'
@@ -329,12 +342,9 @@ def main():
     #
     # print editdistance.eval(list1, list2)
 
-    # ------------------------------ For debugging ------------------------------ #
+    # ---------------------------- For debugging ---------------------------- #
 
     print "Stop..."
-
-
-
 
 
 
